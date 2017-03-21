6220a8b17add49e0f4189f502bc5321995acbead
John McGuinness <johnmcguinness@johns-mbp.dhcp.nd.edu>
2017-03-20 22:37:16 -0400

    beginnings of Game class

---
+++ Game.h
@@ -0,0 +1,30 @@
+// Game.h
+//This is the interface for the game class for our simulated poker game
+
+#include <vector>
+#include "CardDeck.h"
+
+#ifndef CARDDECK_H
+#define CARDDECK_H
+
+struct Player {
+    double money;
+    vector<Card> hand;
+    
+    void resetHand() {
+        hand.clear();
+    }
+}
+
+class Game{
+
+    public:
+        Game();
+        ~Game();
+        void playHand();
+    private:
+        int numPlayers;
+        //many helper functions to be added
+};
+
+#endif
